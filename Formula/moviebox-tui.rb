dc3b7d31eff4476bbe0f529770cb598816b8b146
mesamirh <73069638+mesamirh@users.noreply.github.com>
2026-08-25 19:37:30 +0000

    chore(homebrew): update formula to v0.1.14

--- Formula/moviebox-tui.rb
+++ Formula/moviebox-tui.rb
@@ -1,8 +1,8 @@
 class MovieboxTui < Formula
-  VERSION = "0.1.13"
-  MACOS_SHA256 = "33fd8177fcfb244df3971287b0aa9c584a302493ef7b11eee8d6f4de607614db"
-  LINUX_X64_SHA256 = "7caa0b96e91a060d9677135de742e47a26a6b5e58547ce8ebfd25882a1520beb"
-  LINUX_ARM64_SHA256 = "e007faf963de0408c62f12900bf704b26c2ba49be2e3ac081697490939267c84"
+  VERSION = "0.1.14"
+  MACOS_SHA256 = "dafae78848e04678dc736773e3390f7773b24cd7fa98e0385f29e483dd6bc389"
+  LINUX_X64_SHA256 = "94279263008b9f843b6981880e733ca0a4d1387ed7597f7604518be57f3dce72"
+  LINUX_ARM64_SHA256 = "d588b1dee1c484950bf33d89ce69bf71c3f99221795aa0b0250a5e935f5a7166"
 
   desc "Stream movies, shows, anime, and live TV from your terminal"
   homepage "https://github.com/mesamirh/MovieBox-Tui"
